194d610e19fbeb0eb82c7baf64332000df791c8d
milind.technopreneur@gmail.com <milind.technopreneur@gmail.com>
2019-01-23 23:09:29 +0530

    first commit

--- src/main/java/com/example/demo/ConsulAppApplication.java
+++ src/main/java/com/example/demo/ConsulAppApplication.java
@@ -2,10 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ConsulAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/StudentServiceDelegate.java
+++ src/main/java/com/example/demo/StudentServiceDelegate.java
@@ -8,26 +8,45 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
  
 @Service
 public class StudentServiceDelegate
 {
-    @Autowired
+	@Autowired
     RestTemplate restTemplate;
      
-    public String callStudentServiceAndGetData(String schoolname)
-    {
-        System.out.println("Consul Demo - Getting School details for " + schoolname);
+    @HystrixCommand(fallbackMethod = "callStudentServiceAndGetData_Fallback")
+    public String callStudentServiceAndGetData(String schoolname) {
  
-        String response = restTemplate.exchange("http://student-service/getStudentDetailsForSchool/{schoolname}",                                       HttpMethod.GET, null, new ParameterizedTypeReference<String>() {},                                                schoolname).getBody();
-         
+        System.out.println("Getting School details for " + schoolname);
+        String url = "http://localhost:9098/getStudentDetailsForSchool/{schoolname}";
+ 
+        String response = restTemplate
+                .exchange(url
+                , HttpMethod.GET
+                , null
+                , new ParameterizedTypeReference<String>() {
+            }, schoolname).getBody();
+ 
         System.out.println("Response Received as " + response + " -  " + new Date());
  
-        return "School Name -  " + schoolname + " :::  Student Details " + response + " -  " + new Date();
+        return "NORMAL FLOW !!! - School Name -  " + schoolname + " :::  " +
+                    " Student Details " + response + " -  " + new Date();
     }
      
+    @SuppressWarnings("unused")
+    private String callStudentServiceAndGetData_Fallback(String schoolname) {
+ 
+	        System.out.println("Student Service is down!!! fallback route enabled...");
+	 
+	        return "CIRCUIT BREAKER ENABLED!!! No Response From Student Service at this moment. " +
+	                    " Service will be back shortly - " + new Date();
+    }
+ 
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
